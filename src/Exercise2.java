c3434d4aa1333ea8d440d0af525e7f17fddc85bb
kobed6328 <kobed6328@HRH-IC0027337.wrdsb.ca>
2015-09-11 10:50:09 -0400

    Problem1-update

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -18,12 +18,10 @@ public class Exercise2 {
     
     public static int sumDigits(int n)
     {
-        
         if (n < 10)
         {
             return n;
         }
-        
         return n%10 + sumDigits(n/10);
     }
             
