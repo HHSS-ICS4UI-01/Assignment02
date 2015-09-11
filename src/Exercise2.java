46b29fbd853a0f1317e126a8cacad8dbb119f1d9
kobed6328 <kobed6328@HRH-IC0027337.wrdsb.ca>
2015-09-11 10:49:29 -0400

    Problem1

---
+++ src/Exercise2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kobed6328
+ */
+public class Exercise2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(sumDigits(126));
+    }
+    
+    public static int sumDigits(int n)
+    {
+        
+        if (n < 10)
+        {
+            return n;
+        }
+        
+        return n%10 + sumDigits(n/10);
+    }
+            
+}
