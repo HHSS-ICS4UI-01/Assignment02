36e2d7a5d653ffad9953919289da817a1b1e0da4
janaj4926 <janaj4926@HRH-IC0027341.wrdsb.ca>
2015-09-16 11:13:10 -0400

    more more recursion!!!!!

--- src/Problem3.java
+++ src/Problem3.java
@@ -14,6 +14,16 @@ public class Problem3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(binaryConvert(156));
+        
+    }
+    static String binaryConvert(int n){
+        if (n == 1){
+            return "1";
+        }
+        if (n == 0){
+            return "0";
+        }
+        return binaryConvert(n/2) + n%2;
     }
 }
