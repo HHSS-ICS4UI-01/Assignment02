d0d57e4084630c1d0943eca9fe8a26f71b9cff6f
hadik9595 <hadik9595@HRH-IC0027323.wrdsb.ca>
2015-09-18 14:22:17 -0400

    Updated castle

--- src/A2Q5.java
+++ src/A2Q5.java
@@ -1,6 +1,7 @@
 
 import becker.robots.City;
 import becker.robots.Direction;
+import becker.robots.Robot;
 import becker.robots.Thing;
 import becker.robots.Wall;
 
@@ -22,6 +23,9 @@ public class A2Q5 {
     public static void main(String[] args) {
         // TODO code application logic here
         City kw = new City();
+        Robot karel = new Robot(kw,1,3, Direction.SOUTH);
+        Robot tina = new Robot(kw,1,3, Direction.SOUTH);
+        new Wall(kw,10,3, Direction.SOUTH);
         new Thing(kw,1,2);
         new Thing(kw,2,1);
         new Thing(kw,3,1);
@@ -34,6 +38,29 @@ public class A2Q5 {
         new Thing(kw,9,2);
         new Thing(kw,10,2);
         new Thing(kw,10,1);
+        new Thing(kw,2,3);
+        new Thing(kw,2,4);
+        new Thing(kw,2,5);
+        new Thing(kw,2,6);
+        new Thing(kw,3,7);
+        new Thing(kw,3,5);
+        new Thing(kw,3,3);
+        new Thing(kw,5,3);
+        new Thing(kw,5,4);
+        new Thing(kw,5,5);
+        new Thing(kw,8,3);
+        new Thing(kw,8,4);
+        new Thing(kw,9,4);
+        new Thing(kw,9,5);
+        new Thing(kw,9,7);
+        new Thing(kw,8,8);
+        new Thing(kw,8,9);
+        
+        
+        
+        
+        
+        
         
         new Wall(kw,1,1, Direction.WEST);
         new Wall(kw,2,1, Direction.WEST);
@@ -89,6 +116,8 @@ public class A2Q5 {
         new Wall(kw,9,6, Direction.SOUTH);
         new Wall(kw,9,5, Direction.SOUTH);
         new Wall(kw,9,4, Direction.SOUTH);
+        
+        
         
         
         
