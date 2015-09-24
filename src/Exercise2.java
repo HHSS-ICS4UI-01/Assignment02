5dce0149ff1a1922eb319b06d2b76920c3be6286
donet6376 <donet6376@HRH-IC0023631.wrdsb.ca>
2015-09-24 14:21:07 -0400

    Started Problem4

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -58,6 +58,61 @@ public class Exercise2 {
        }
     }
     
+    static String convert(int n, int b)
+    {
+        if(n == 0)
+        {
+            return "0";
+        }if(n == 1)
+        {
+            return "1";
+        }if(n == 2)
+        {
+            return "2";
+        }if(n == 3)
+        {
+            return "3";
+        }if(n == 4)
+        {
+            return "4";
+        }if(n == 5)
+        {
+            return "5";
+        }if(n == 6)
+        {
+            return "6";
+        }if(n == 7)
+        {
+            return "7";
+        }if(n == 8)
+        {
+            return "8";
+        }if(n == 9)
+        {
+            return "9";
+        }if(n == 10)
+        {
+            return "A";
+        }if(n == 11)
+        {
+            return "B";
+        }if(n == 12)
+        {
+            return "C";
+        }if(n == 13)
+        {
+            return "D";
+        }if(n == 14)
+        {
+            return "E";
+        }if(n == 15)
+        {
+            return "F";
+        }
+        
+    }
+    
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         //Problem 1
