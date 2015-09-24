932724ad04f9aeeb6674213be75fe077cdbb9381
donet6376 <donet6376@HRH-IC0023631.wrdsb.ca>
2015-09-24 13:43:30 -0400

    Problems 1 to 7 completed.

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -16,15 +16,15 @@ public class Exercise2 {
      * @param args the command line arguments
      */
     static int sumDigits(int n)
-    {
+    {   //if the number is 1 digit long
         if(n >= 0 && n <=9)
         {
-            return n;
+            return n; //return that digit
         }else
-        {
-            int q = n / 10;
-            int r = n % 10;
-            return r + sumDigits(q);
+        {   //remove the last digit from the number
+            int q = n / 10; //set the removed digit
+            int r = n % 10; //set the remainder
+            return r + sumDigits(q); //loop 
         }
     }
     
@@ -39,9 +39,23 @@ public class Exercise2 {
         }
     }
     
-    static int binaryConvert(int n)
+    static String binaryConvert(int n)
     {
-       return 1;
+       if(n == 0)
+       {
+           return "0";
+       }if(n == 1)
+       {
+           return "1";
+       }
+       if(n % 2 == 0)
+       {
+           return (binaryConvert(n/2) + "0");
+       }
+       else
+       {
+           return (binaryConvert(n/2) + "1");
+       }
     }
     
     public static void main(String[] args) {
@@ -49,18 +63,18 @@ public class Exercise2 {
         //Problem 1
         //System.out.println("Enter in a number to receive it's sum of digits:");
         //int num = input.nextInt();
-        //System.out.println("The sum of " + num + " is " + sumDigits(num));
+        //System.out.println("The sum of " + num + "'s digits is " + sumDigits(num));
         
         //Problem 2
         //System.out.println("Enter in the amount of rows:");
         //int num = input.nextInt();
         //System.out.println("The amount of blocks in a triangle with " + num + 
-                //" rows is " + triangle(num));
+        //" rows is " + triangle(num));
         
         //Problem 3
         System.out.println("Enter in a number to convert to binary");
         int num = input.nextInt();
-        System.out.println();
+        System.out.println(num + " in binary is " + binaryConvert(num));
         
     }
 }
